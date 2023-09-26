ba8f65076040ce1ee2eadbb665bc6027b7f093f2
Gansey14 <hlousek.samuel14@gmail.com>
2023-09-26 16:45:41 +0200

    Portfolio 26/9/2023

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Variables
+
 const menu2 = document.getElementById("menu2");
 const navigation = document.querySelector(".navigation");
 const logo = document.querySelector("#logo")
@@ -7,11 +9,24 @@ const ball1 = document.querySelector("#ball1");
 const html = document.querySelector("html");
 const textColorVariable = "--text";
 const backgroundColorVariable = "--background";
+const body = document.body;
+const blurButton = document.getElementById('blurButton');
+const moreButton = document.getElementById('btn-detail');
+const detailSection = document.querySelector('.detail');
+const linktodetail = document.querySelector('#linktodetail');
+const chevron = document.querySelector('.fa-chevron-right');
+const changeColorWord = document.querySelector('#changeColorWord');
+const linkNavWork = document.querySelector('#linkNavWork');
+const linkNavAbout = document.querySelector('#linkNavAbout');
+const linkNavContact = document.querySelector('#linkNavContact');
+const breakpoint = 1100;
 
-const backgroundColors = ["#EBEDED", "#f1fffa", "#282B29", "#F4C4C4"]
-const textColors = ["#E83C38", "#3C4F76", "#F4C4C4", "#282B29"]
+const backgroundColors = ["#EBEDED", "#f1fffa", "#282B29", "#F4C4C4", "#0C0910"]
+const textColors = ["#E83C38", "#3C4F76", "#F4C4C4", "#282B29", "#d6bcfb"]
 
-menu2.onclick = function () {
+// Nav functionality
+
+function toggleNav() {
     menu2.classList.toggle("openmenu2");
     navigation.classList.toggle("opennav");
     logo.classList.toggle("logogone");
@@ -20,19 +35,57 @@ menu2.onclick = function () {
     html.classList.toggle("noscrollburger");
 }
 
+menu2.onclick = toggleNav;
+
+if (window.innerWidth <= breakpoint) {
+    linkNavWork.addEventListener('click', toggleNav);
+    linkNavAbout.addEventListener('click', toggleNav);
+    linkNavContact.addEventListener('click', toggleNav);
+}
+
+// Changing color themes
+
 let number = 1;
 
-html.addEventListener("click", _ => {
-    if (number < 3) {
+changeColorWord.addEventListener("click", _ => {
+    if (number < 4) {
         number += 1;
     } else (
-        number = number - 3
+        number = number - 4
     )
     console.log(number);
     document.documentElement.style.setProperty(backgroundColorVariable, backgroundColors[number]);
     document.documentElement.style.setProperty(textColorVariable, textColors[number]);
 })
 
+// Blur effect
+
+blurButton.addEventListener('click', () => {
+    // Toggle the 'blurred' class on the body element
+    body.classList.toggle('blurred');
+});
+
+// Remove the blur when clicking anywhere else on the document
+document.addEventListener('click', (event) => {
+    if (!blurButton.contains(event.target)) {
+        body.classList.remove('blurred');
+    }
+});
+
+// Hide and unhide "Find out more" section
+
+moreButton.addEventListener('click', () => {
+    // Check if the element is currently hidden
+    if (detailSection.style.display === 'none') {
+        // If hidden, show the element
+        detailSection.style.display = 'flex';
+        linktodetail.innerText = "Hide detail";
+    } else {
+        // If visible, hide the element
+        detailSection.style.display = 'none';
+        linktodetail.innerText = "Find out more";
+    }
+});
 
 
 
